afc812fb1dd8a7f0ffbcfe8659e1abedc0c4d10e
Vika Rogozina <rogozina.vv93@gmail.com>
2020-04-02 20:27:41 +0300

    git ignor added & checking figures 50%

--- oop/oop.py
+++ oop/oop.py
@@ -1,6 +1,5 @@
 import turtle
 import math
-
 turtle.shape('turtle')
 
 class figure:
@@ -96,34 +95,121 @@ class triangle(figure):
         turtle.setpos (x_center, y_center)
         #turtle.mainloop()
 
-fff = []
-def sbor_dannih():
-    n = int(input ("Я могу нарисовать столько фигур, сколько хочешь... только скажи сколько?"))
-    for i in range(1, n+1):
-        type_of_figure = int(input("1 - Круг, 2 - Прямоугольник, 3 - Треугольник. Выбирай?"))
-        if type_of_figure == 1:
-            x, y, r = (int(j) for j in input('Введи через прообел координаты начальной точки и радиус').split())
-            fff.append(circle(x, y, r))
-        elif type_of_figure == 2:
-            x, y, a, b = (int(j) for j in input('Введи через прообел координаты начальной точки, длину и ширину').split())
-            fff.append(rectangle(x, y, a, b))
-        elif type_of_figure == 3:
-            x, y, a, b = (int(j) for j in input('Введи через прообел координаты начальной точки, длину основания и высоту').split())
-            fff.append(triangle(x, y, a, b))
+# fff = []
+
+# def data_collection():
+#     n = int(input ("Я могу нарисовать столько фигур, сколько хочешь... только скажи сколько?"))
+#     for i in range(1, n+1):
+#         type_of_figure = int(input("1 - Круг, 2 - Прямоугольник, 3 - Треугольник. Выбирай?"))
+#         if type_of_figure == 1:
+#             x, y, r = (int(j) for j in input('Введи через прообел координаты начальной точки и радиус').split())
+#             fff.append(circle(x, y, r))
+#         elif type_of_figure == 2:
+#             x, y, a, b = (int(j) for j in input('Введи через прообел координаты начальной точки, длину и ширину').split())
+#             fff.append(rectangle(x, y, a, b))
+#         elif type_of_figure == 3:
+#             x, y, a, b = (int(j) for j in input('Введи через прообел координаты начальной точки, длину основания и высоту').split())
+#             fff.append(triangle(x, y, a, b))
+#         else:
+#             print ("только 1, 2 или 3. Никакой тебе фигуры")
+#             fff.append(0)
+#     return fff, n
+
+# data collection()
+# print (fff)
+# n = fff.__len__()
+
+# def drawing():
+#     for elem in range(1,n+1):
+#         fff[elem-1].draw_figure('blue')
+#     return None
+
+# drawing()
+f1 = circle(50, 20, 60)
+f2 = rectangle(100, 10, 200, 60)
+f3 = rectangle(200, 0, 50, 40)
+f4 = circle(20 , 10, 60)
+f5 = triangle(40, 40, 60, 45)
+fig = [f1, f2, f3, f4, f5]
+
+# for i in range (0,len(fig)):
+#     fig[i].draw_figure('red')
+f1.draw_figure('red')
+f2.draw_figure('black')
+def proverka(a, b):
+    if a.__class__.__name__ == b.__class__.__name__ == "circle":
+        if math.fabs(a.lenght_x - b.lenght_x) <= math.sqrt((a.x_center - b.x_center) ** 2 + (a.y_center + a.lenght_x - b.y_center - b.lenght_x) ** 2) <= (a.lenght_x + b.lenght_x):
+            print ('фигуры пересекаются ')
         else:
-            print ("только 1, 2 или 3. Никакой тебе фигуры")
-            fff.append(0)
-    return fff, n
+            print ('фигуры НЕЕЕ пересекаются ')
 
-sbor_dannih()
-print (fff)
-n = fff.__len__()
-def drawing():
-    for elem in range(1,n+1):
-        fff[elem-1].draw_figure('blue')
-    return None
+    elif a.__class__.__name__ == b.__class__.__name__ == "rectangle":
+        x1 = a.x_center
+        y1 = a.y_center
+        lx1 = a.lenght_x
+        ly1 = a.lenght_y
+        x2 = b.x_center
+        y2 = b.y_center
+        lx2 = b.lenght_x
+        ly2 = b.lenght_y
+        usl = bool(x1 < (x2 or (x2 and (x2+lx2)) or (x2+lx2)) < (x1 + lx1))
+        usl1 = bool((y1 < y2 < (y1 + ly1)) and  (y1 + ly1) < (y2 + ly2))
+        usl2 = bool(y2 < y1 and ((y1 + ly1) < (y2 + ly2)))
+        usl3 = bool(y2 < y1 and (y1 < (y2 + ly2) < (y1 + ly1)))
+        usl4 = bool(y2 > y1 and ((y2 + ly2) < (y1+ ly1)))
+        usl5 = bool((x2 < x1 and ((x1 + lx1) < (x + lx))) and ((y1 < y2 < (y1+lx1) and ((y1 + ly1) < (y2 + ly2)) or ((y1 < (y2 + ly2) < (y1 + ly1)) and (y2 < y1)))))
+        if (usl and (usl1 or usl2 or usl3 or usl4)) or usl5 :
+            print ("пересекаются ")
+        else:
+            print ("НЕ пересекаются")
 
-drawing()
+    elif a.__class__.__name__ == "circle" and b.__class__.__name__ == "rectangle" or b.__class__.__name__ == "circle" and a.__class__.__name__ == "rectangle":
+        if b.__class__.__name__ == "circle":
+            xc = b.x_center # a это прямоугольник
+            yc = b.y_center + b.lenght_x
+            rad = b.lenght_x
+            xr = a.x_center
+            yr = a.y_center
+            xlen = a.lenght_x
+            ylen = a.lenght_y
+        else: # a это круг
+            xc = a.x_center
+            yc = a.y_center + a.lenght_x
+            rad = a.lenght_x
+            xr = b.x_center
+            yr = b.y_center
+            xlen = b.lenght_x
+            ylen = b.lenght_y
+        usl = xr < xc < (xr + xlen) and (math.fabs(yc - (yr + ylen)) < rad  or math.fabs(yc - yr) < rad)
+        usl2 = yr < yc < (yr + ylen) and (math.fabs(xc - (xr + xlen)) < rad  or math.fabs(xc - xr) < rad)
+        usl3 = math.sqrt((xr - xc)**2 + (yr - yc)**2) < rad
+        usl4 = math.sqrt((xr + xlen - xc)**2 + (yr - yc)**2) < rad
+        usl5 = math.sqrt((xr + xlen - xc)**2 + (yr + ylen - yc)**2) < rad
+        usl6 = math.sqrt((xr - xc)**2 + (yr + ylen - yc)**2) < rad
+        if  usl or usl2 or usl3 or usl4 or usl5 or usl6:
+            print ("пересекаются ")
+        else:
+            print ("НЕ пересекаются")
+    else: 
+        print ('еще не дописала')
+
+    # elif a.__class__.__name__ == b.__class__.__name__ == "triangle"
+    # elif a.__class__.__name__ == b.__class__.__name__ == 
+    # elif a.__class__.__name__ == b.__class__.__name__ == 
+    # elif a.__class__.__name__ == b.__class__.__name__ == 
+
+proverka(f1, f2)
+
+# def method_test(): #проверяем пересекается ли пара фигур
+#     for i in range(1, len(fig)-1):
+#         for j in range (2, len(fig)):
+#             a = fig[i]
+#             b = fig[j]
+#             proverka(a,b)
+
+
+
+
 
 # circle_first = circle(50, 50, 35)
 # print (circle_first.parametrs_of_figure())
